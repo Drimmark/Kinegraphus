50656a748caa58e43b7215a654c0d3785fe60c29
Patataman <shas_arroyo@hotmail.com>
2016-04-11 14:47:55 +0200

    estilo index

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-from models.basicmodel import db
+#from models.basicmodel import db
 
 app = Flask(__name__)
-app.config.from_pyfile('config_file.cfg')
-db.app = app
-db.init_app(app)
+#app.config.from_pyfile('config_file.cfg')
+#db.app = app
+#db.init_app(app)
 
 
 @app.route('/')
