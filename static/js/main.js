651e9bf96b412d16f4405e71a8e43512a511e472
Patataman <lezdshas@gmail.com>
2016-04-14 01:54:03 +0200

    index

--- static/js/main.js
+++ static/js/main.js
@@ -10,25 +10,50 @@ $(document).ready(function () {
 		request.done(function (response, textStatus, jqXHR){
 			lista = [];
 			console.log(response);
-			for (i = 0; i < response.length && i < 10; i++) {
+			for (i = 0; i < response.length; i++) {
 				lista.push(response[i].name);
 			}
-			console.log(lista);
 			$( "#inputText" ).autocomplete({
 				minLength: 3,
 				source: lista
    			});
+
 		});
 
+    
 		// Callback handler that will be called on failure
 		request.fail(function (jqXHR, textStatus, errorThrown){
-		    // Log the error to the console
-		    console.error(
-		        "The following error occurred: "+
-		        textStatus, errorThrown
-		    );
+			// Log the error to the console
+			console.error(
+			"The following error occurred: "+
+			textStatus, errorThrown
+			);
 		});
+    
+	});
 
+	$('#busqueda').click(function() {
+		console.log($('#inputText').val());
+		
+		request = $.ajax({
+			url: "/searchsuggestions",
+			type: "get",
+			data: "term="+$('#inputText').val()
+		});
+		
+		// Callback handler that will be called on success
+		request.done(function (response, textStatus, jqXHR){
+			lista = [];
+			console.log(response);
+			for (i = 0; i < response.length; i++) {
+				lista.push(response[i].name);
+			}
+			$( "#inputText" ).autocomplete({
+				minLength: 3,
+				source: lista
+   			});
+
+		});
 	});
 
 });
