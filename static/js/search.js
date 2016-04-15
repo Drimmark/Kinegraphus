403b319544407e68b011f9eda9f8a60a3d147f9b
Seind <adri@Freud.Freud.org>
2016-04-15 03:17:05 +0200

    git se ha puesto tonto

--- static/js/search.js
+++ static/js/search.js
@@ -9,3 +9,27 @@ $(function() {
         'autoclose': true
     })
 })
+
+$(document).ready(function(){
+ 
+	$(window).scroll(function(){
+		x = document.getElementById('search').offsetHeight;
+		//$(this).scrollTop() < footer
+		if( $(this).scrollTop() < 106){//Estamos arriba
+			//$('#map').removeClass('map3');
+			$('#map').removeClass('map2');
+			$('#map').addClass('map');
+		}	
+		else if( $(this).scrollTop() > 106 && $(this).scrollTop() < x-400){//estamos en medio
+			$('#map').removeClass('map3');
+			$('#map').removeClass('map');
+			$('#map').addClass('map2');
+			;	
+		} else if ($(this).scrollTop() > x-400){//estamos abajo
+			//$('#map').removeClass('map');
+			$('#map').removeClass('map2');
+			$('#map').addClass('map3')
+		}
+	});
+ 
+});//section#search .map
