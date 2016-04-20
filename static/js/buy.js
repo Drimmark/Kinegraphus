b3c2004ea966f29d16946827c72c9841f085c296
Patataman <lezdshas@gmail.com>
2016-04-20 02:44:04 +0200

    salas

--- static/js/buy.js
+++ static/js/buy.js
@@ -1,13 +1,29 @@
 $(function() {
+
+	//Generación de la sala
 	for (i in sala) {
 		ul = $("<ul>",{'class':'fila'}).appendTo("#salas");
 		for (k in sala[i]) {
 			if (sala[i][k] == 0) {
-				$("<li>").html('<i class="fa fa-circle-o" aria-hidden="true"></i>').appendTo(ul);
+				$("<li>").html('<i class="fa fa-circle verde" aria-hidden="true"></i>').appendTo(ul);
 			} else {
-				$("<li>").html('<i class="fa fa-circle" aria-hidden="true"></i>').appendTo(ul);
+				$("<li>").html('<i class="fa fa-circle rojo" aria-hidden="true"></i>').appendTo(ul);
 			}
-
 		}
 	}
+
+	$('#salas ul li .verde').click(function(){
+		$(this).toggleClass('verde');
+		$(this).toggleClass('amarillo');
+	});
+
+	$('#salas ul li .amarillo').click(function(){
+		$(this).toggleClass('verde');
+		$(this).toggleClass('amarillo');
+	});
+
+	//Hay que actualizar tambien el número de entradas.
+	$('#showButacas').click(function(){
+		$('#butacas').slideToggle('horizontal','slow');
+	});
 });
