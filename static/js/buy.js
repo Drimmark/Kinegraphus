7366fa8226fc2ba605c0b38f61661a69b6b55a81
Patataman <lezdshas@gmail.com>
2016-04-23 19:11:15 +0200

    actualizar numero entradas

--- static/js/buy.js
+++ static/js/buy.js
@@ -6,24 +6,36 @@ $(function() {
 		for (k in sala[i]) {
 			if (sala[i][k] == 0) {
 				$("<li>").html('<i class="fa fa-circle verde" aria-hidden="true"></i>').appendTo(ul);
-			} else {
+			} else if (sala[i][k] == 1) {
 				$("<li>").html('<i class="fa fa-circle rojo" aria-hidden="true"></i>').appendTo(ul);
+			} else {
+				$("<li>").html('<i class="fa fa-circle amarillo" aria-hidden="true"></i>').appendTo(ul);
 			}
 		}
 	}
 
-	$('#salas ul li .verde').click(function(){
-		$(this).toggleClass('verde');
-		$(this).toggleClass('amarillo');
-	});
-
-	$('#salas ul li .amarillo').click(function(){
-		$(this).toggleClass('verde');
-		$(this).toggleClass('amarillo');
+	$('div#salas ul li i').click(function(){
+		if (this.classList[2] != 'rojo') {
+			$(this).toggleClass('verde');
+			$(this).toggleClass('amarillo');
+			if (this.classList[2] == 'verde') {
+				$('#numButacas').html(parseInt($('#numButacas').html()) - 1 ); 
+			} else {
+				$('#numButacas').html(parseInt($('#numButacas').html()) + 1 ); 
+			}
+		}
 	});
 
 	//Hay que actualizar tambien el número de entradas.
 	$('#showButacas').click(function(){
-		$('#butacas').slideToggle('horizontal','slow');
+		if ( $('#butacas').css('width') != '0px'){
+			$('#showButacas').removeClass("fa fa-minus-square");
+			$('#showButacas').addClass("fa fa-plus-square");
+			$('#butacas').css('width', '0%');
+		} else {
+			$('#showButacas').removeClass("fa fa-plus-square");
+			$('#showButacas').addClass("fa fa-minus-square");
+			$('#butacas').css('width', '100%');
+		}
 	});
 });
